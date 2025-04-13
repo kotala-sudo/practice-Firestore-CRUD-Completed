6d6a97c32e894fbd4ace20d4721dc30bacd528da
Your Name <you@example.com>
2025-04-13 17:46:13 -0500

    Adding the completed project demonstrating CRUD operations on Angular Firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { User, UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'practice-firestore-crud';
+
+  // Injecting UserService from user.service.ts
+  userService = inject(UserService);
+
+  // This is the user object that will be used in the form
+  user: User = {
+    id: '',
+    name: '',
+    email: ''
+  }
+
+  // This is the id of the user that will be edited
+  editUserId!: string | null;
+
+  // This is the array of users that will be displayed in the template
+  users: User[] = []
+
+
+  // This is the hook method that will be called when the component is initialized
+  ngOnInit(){
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
+
+
+  addUser(){
+    this.userService.addUser(this.user);
+  }
+
+
+  // This method will be called when the user clicks on the edit button
+  // It will set the editUserId to the id of the user that will be edited
+  setEditUser(editUser: User){
+    this.editUserId = editUser.id ?? null;
+    this.user = { ... editUser}
+  }
+
+  // This method will be called when the user clicks on the update button
+  // It will call the updateUser method from the userService and pass the user object to it 
+  updateUser(user: User){
+    this.userService.updateUser(user);
+    this.editUserId = null;
+    this.user =  {
+    id: '',
+    name: '',
+    email: ''
+  }
+  }
+
+  // This method will be called when the user clicks on the delete button
+  deleteUser(id:string){
+    this.userService.deleteUser(id);
+  }
+
+  // This method will be called when the user clicks on the reset button
+  resetForm(){
+    this.user = {
+    id: '',
+    name: '',
+    email: ''
+  };
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Import the functions you need from the SDKs you need
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration firebaseConfig goes here
+// Copy the firebaseConfig object from your Firebase project settings
+// const firebaseConfig = { ...... }
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    //make sure to copy and paste firebaseConfig object from your Firebase project settings above
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  
+  ]
 };
